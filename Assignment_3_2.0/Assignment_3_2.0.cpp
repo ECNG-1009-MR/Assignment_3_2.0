a0949b004010c069cbf16a43f10b6abfde0c25f1
Nathan Ragoobar <nathan.ragoobar@my.uwi.edu>
2022-03-27 00:03:49 -0400

    srjgbf

--- Assignment_3_2.0/Assignment_3_2.0.cpp
+++ Assignment_3_2.0/Assignment_3_2.0.cpp
@@ -97,7 +97,7 @@ std::vector<std::vector<int>> isNaN(std::vector<std::vector<std::string>>& rawDa
 
 std::vector<std::vector<int>> readfile()
 {
-	std::vector<std::vector<std::string>> rawData;
+	std::vector<std::vector<std::string>> rawData; //Creates 2D vector to store values
 
 	std::ifstream readfile;
 
@@ -152,7 +152,6 @@ std::vector<std::vector<int>> readfile()
 
 
 
-	std::vector<int> data = { 1,2 };
 	return isNaN(rawData);
 }
 
